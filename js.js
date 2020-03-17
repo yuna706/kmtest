aeb0700f7c70aafe6d7de68071a2d3c67976beb9
yuna706 <riyu6027@naver.com>
2020-03-18 00:39:37 +0900

    넘어가기, 그래프

--- js.js
+++ js.js
@@ -1,3 +1,33 @@
 $(function(){
-    
+    $("button").click(function(){
+        $(this).fadeOut(500);
+        setTimeout(function(){
+            $("#q1").fadeIn(500);
+            },500
+        )
+    })
+
+    let total = [0,0,0,0];
+    $(".list> li").click(function(){
+        let ch = $(this).val();
+        total[ch]++;
+        let boomo = $(this).parents(".questions");
+        let next = boomo.next();
+        let next_grap = next.find(".grap> div");
+        let grap_width = next_grap.text();
+
+        console.log(grap_width);
+
+        boomo.fadeOut(500);
+        setTimeout(function(){
+            $(next).fadeIn(500);
+            },500
+        )
+        setTimeout(function(){
+            $(next_grap).animate({"width": grap_width}, 500);
+        }, 500)
+    })
+    $("#q5").click(function(){
+        
+    })
 })
