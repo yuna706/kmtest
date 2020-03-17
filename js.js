9aa4a0d68d987c733f04b58dd060cc8902367ccd
yuna706 <riyu6027@naver.com>
2020-03-18 01:22:08 +0900

    구원의 댄스

--- js.js
+++ js.js
@@ -3,29 +3,29 @@ $(function(){
         $(this).fadeOut(500);
         setTimeout(function(){
             $("#q1").fadeIn(500);
-            },500
-        )
+        },500);
     })
 
     let total = [0,0,0,0];
     $(".list> li").click(function(){
         let ch = $(this).val();
         total[ch]++;
+
         let boomo = $(this).parents(".questions");
         let next = boomo.next();
         let next_grap = next.find(".grap> div");
         let grap_width = next_grap.text();
 
-        console.log(grap_width);
-
         boomo.fadeOut(500);
         setTimeout(function(){
             $(next).fadeIn(500);
-        },500)
+        },500);
+
         setTimeout(function(){
             $(next_grap).animate({"width": grap_width}, 500);
-        }, 500)
+        }, 500);
     })
+
     $("#q5").click(function(){
         let last_width = $("#loading> div").text();
 
@@ -39,4 +39,24 @@ $(function(){
             $(".result-view").fadeIn(500);
         }, 2500);
     })
+
+    let max = total[0];
+    let j = 0;
+    for(let i = 1; i < 5; i++){
+        if(max < tatal[i]){
+            max = tatal[i];
+            j++;
+        }
+    }
+    
+    let depar = $(".result-view> p");
+    let ex = $(".result-view> div");
+
+    console.log(j);
+    switch(j){
+        case 0:
+            depar.text("마케팅 경영과");
+            ex.html('마케팅경영과에서는 경영관련 분야의 기초 전문지식과 현장 실무능력을 배우며, 세무, 회계사무 분야 도제학교를 운영합니다. 금융, 회계, 마케팅 등 경영관련 직업 분야의 직무수행에 필요한 기초 전문지식과 현장 실무능력을 배우며, 산업현장과 교육과정을 연계하여 세무, 회계사무 분야 기업 맞춤형 인재를 기릅니다. <img src="img/ma1.png" alt="마케팅 경영과 사진1"> <img src="img/ma2.png" alt="마케팅 경영과 사진2">');
+            break;
+        }
 })
